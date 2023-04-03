074217055736d5627d356d118b245d1238aec6ef
hafidaouzi0 <faouzibrahim2014@gmail.com>
2023-04-03 22:46:34 +0000

    commit

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,12 +1,35 @@
 package com.example.demo.student;
 
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
 
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class StudentConfig {
+
+    //on va creer qlq utilisateurs students pour tester le comportement de notre application
+    private final StudentRepository studentRepository;
+
+
+    @Bean
+    CommandLineRunner initStudent(){
+
+        return args -> {
+           studentRepository.save(new Student(
+                   null,
+                   "hafida",
+                   "hafida@gmaul.com",
+                   LocalDate.of(2000,9,25)
+
+           ));
+        };
+    }
+
 
 }
