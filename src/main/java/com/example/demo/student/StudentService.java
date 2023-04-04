244882075a65640ab733975cd8174cd86a6c4f37
hafidaouzi0 <faouzibrahim2014@gmail.com>
2023-04-04 02:02:28 +0000

    add student service

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.demo.student;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface StudentService {
+
+    Student addStudent(Student student) throws Exception ;
+    Student updateStudent(Student student) throws Exception;
+    Student deleteStudent(Student student) throws Exception;
+    Student getStudent(Long id) throws Exception;
+    List<Student> getAllStudents() throws Exception;
+Student getStudentByEmail(String email) throws Exception;
+
+}
---
+++ src/main/java/com/example/demo/student/StudentServiceImp.java
@@ -0,0 +1,57 @@
+package com.example.demo.student;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor //pour l'injection
+public class StudentServiceImp implements  StudentService{
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public Student addStudent(Student student) throws Exception {
+        //on ajoute les containtes  meties dans cahiers de charge avant d'enregistrer student
+        if(student!=null && student.getCode()!=null)
+         return studentRepository.save(student);
+         else
+             throw new RuntimeException("student cannot be added");
+    }
+
+    @Override
+    public Student updateStudent(Student student) throws Exception {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student deleteStudent(Student student) throws Exception {
+                studentRepository.delete(student);
+        return student;
+
+    }
+
+    @Override
+    public Student getStudent(Long id) throws Exception {
+        if(studentRepository.findById(id).isPresent())
+         return studentRepository.findById(id).get();
+        else throw new RuntimeException("student not found");
+    }
+
+    @Override
+    public List<Student> getAllStudents() throws Exception {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student getStudentByEmail(String email) throws Exception {
+        List<Student> students=studentRepository.findStudentByEmail(email);
+        if(students.size()>0)
+         return  students.get(0);
+        else
+            throw new RuntimeException("student not found");
+    }
+}
